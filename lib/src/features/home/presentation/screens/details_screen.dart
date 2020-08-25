f67538f4c16b77bc415079ae4e60ad8f1a990c47
Aayush Jain <f20180320@hyderabad.bits-pilani.ac.in>
2020-08-25 17:39:48 +0530

    add vision

--- lib/src/features/home/presentation/screens/details_screen.dart
+++ lib/src/features/home/presentation/screens/details_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:clay_containers/clay_containers.dart';
+import 'package:cms_distilled/src/utils/res/colors.dart';
 import 'package:cms_distilled/src/utils/text_detector_painter.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class DetailsScreen extends StatefulWidget {
   final File image;
@@ -17,8 +20,9 @@ class _DetailsScreenState extends State<DetailsScreen> {
   _DetailsScreenState(this.imageFile);
 
   Size _imageSize;
-  String recognizedText = "Loading ...";
+  String recognizedText = "Magic is happening...";
   List<TextElement> _elements = [];
+  TextEditingController _controller;
 
   void _initializeVision() async {
     await _getImageSize();
@@ -32,16 +36,28 @@ class _DetailsScreenState extends State<DetailsScreen> {
     String blah = "";
     for (TextBlock block in visionText.blocks) {
       for (TextLine line in block.lines) {
-        print(line.text);
-        blah += line.text + "\n";
-        for (TextElement element in line.elements) {
-          _elements.add(element);
+        // print(line.text);
+        if (!line.text.contains("Le") &&
+            !line.text.contains("ure") &&
+            !line.text.contains("Tut") &&
+            !line.text.contains("Practical") &&
+            !line.text.contains("Laboratory") &&
+            !line.text.contains("Location") &&
+            !line.text.contains("TBA") &&
+            !line.text.contains("AM") &&
+            !line.text.contains("PM") &&
+            !line.text.contains("Aug")) {
+          blah += line.text + "\n";
+          for (TextElement element in line.elements) {
+            _elements.add(element);
+          }
         }
       }
     }
 
     setState(() {
       recognizedText = blah;
+      _controller = TextEditingController(text: recognizedText);
     });
   }
 
@@ -69,78 +85,125 @@ class _DetailsScreenState extends State<DetailsScreen> {
   void initState() {
     _initializeVision();
     super.initState();
+    _controller = TextEditingController(text: recognizedText);
   }
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    BorderRadiusGeometry radius = BorderRadius.only(
+      topLeft: Radius.circular(24.0),
+      topRight: Radius.circular(24.0),
+    );
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Image Details"),
-      // ),
-      body: _imageSize != null
-          ? Stack(
-              children: <Widget>[
-                Center(
-                  child: Container(
-                    width: double.maxFinite,
-                    color: Colors.black,
-                    child: CustomPaint(
-                      foregroundPainter: TextDetectorPainter(
-                        _imageSize,
-                        _elements,
+      body: SlidingUpPanel(
+        defaultPanelState: PanelState.OPEN,
+        borderRadius: radius,
+        panel: Container(
+          decoration: BoxDecoration(
+            color: baseColor,
+            borderRadius: radius,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Icon(
+                      Icons.drag_handle,
+                      color: uiColor,
+                    ),
+                    // SizedBox(height: 5),
+                    Text(
+                      "Identified Subjects",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: AspectRatio(
-                        aspectRatio: _imageSize.aspectRatio,
-                        child: Image.file(
-                          imageFile,
+                    ),
+                  ],
+                ),
+                ClayContainer(
+                  color: baseColor,
+                  emboss: true,
+                  child: TextField(
+                    controller: _controller,
+                    maxLines: 14,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    InkWell(
+                      onTap: () => Navigator.pop(context),
+                      child: ClayContainer(
+                        color: baseColor,
+                        height: 50,
+                        width: width * 0.4,
+                        borderRadius: 100,
+                        curveType: CurveType.concave,
+                        child: Center(
+                          child: ClayText(
+                            "RETAKE",
+                            emboss: true,
+                            textColor: uiColor,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              // color: primaryColor,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    ClayContainer(
+                      color: baseColor,
+                      height: 50,
+                      width: width * 0.4,
+                      borderRadius: 100,
+                      curveType: CurveType.convex,
+                      child: Center(
+                        child: ClayText(
+                          "ENROLL",
+                          // emboss: true,
+                          textColor: primaryColor,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            // color: primaryColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Card(
-                    elevation: 8,
-                    color: Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 8.0),
-                            child: Text(
-                              "Identified text",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            height: 200,
-                            child: SingleChildScrollView(
-                              child: Text(
-                                recognizedText,
-                              ),
-                            ),
-                          ),
-                        ],
+              ],
+            ),
+          ),
+        ),
+        body: Center(
+          child: _imageSize != null
+              ? Container(
+                  width: double.maxFinite,
+                  color: Colors.black,
+                  child: CustomPaint(
+                    foregroundPainter: TextDetectorPainter(
+                      _imageSize,
+                      _elements,
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: _imageSize.aspectRatio,
+                      child: Image.file(
+                        imageFile,
                       ),
                     ),
                   ),
-                ),
-              ],
-            )
-          : Container(
-              color: Colors.black,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            ),
+                )
+              : CircularProgressIndicator(),
+        ),
+      ),
     );
   }
 }
